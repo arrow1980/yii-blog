ffb9f859504d68ddfef825f04a535298d6208ab0
Abner Yu <arrow1980@gmail.com>
2014-03-06 23:41:58 +0800

    Post Management - Creating and Updating Posts complete.

--- protected/models/Post.php
+++ protected/models/Post.php
@@ -19,7 +19,11 @@
  */
 class Post extends CActiveRecord
 {
-	/**
+	const STATUS_DRAFT=1;
+  const STATUS_PUBLISHED=2;
+  const STATUS_ARCHIVED=3;
+  
+  /**
 	 * @return string the associated database table name
 	 */
 	public function tableName()
@@ -35,15 +39,22 @@ class Post extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('title, content, status, author_id', 'required'),
-			array('status, create_time, update_time, author_id', 'numerical', 'integerOnly'=>true),
-			array('title', 'length', 'max'=>128),
-			array('tags', 'safe'),
+			array('title, content, status', 'required'),
+      array('title', 'length', 'max'=>128),
+			array('status', 'in', 'range'=>array(1,2,3)),
+			array('tags', 'match','pattern'=>'/^[\w\s,]+$/',
+          'message'=>'Tag can only contain word characters.'),
+      array('tags', 'normalizeTags'),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
-			array('id, title, content, tags, status, create_time, update_time, author_id', 'safe', 'on'=>'search'),
+			array('title, status', 'safe', 'on'=>'search'),
 		);
 	}
+  
+  public function normalizeTags($attribute,$params)
+  {
+    $this->tags=Tag::array2string(array_unique(Tag::string2array($this->tags)));
+  }
 
 	/**
 	 * @return array relational rules.
@@ -53,8 +64,12 @@ class Post extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-			'comments' => array(self::HAS_MANY, 'Comment', 'post_id'),
 			'author' => array(self::BELONGS_TO, 'User', 'author_id'),
+      'comments' => array(self::HAS_MANY, 'Comment', 'post_id',
+          'condition' => 'comments.status='.Comment::STATUS_APPROVED,
+          'order' => 'comments.create_time DESC'),
+      'commentCount' => array(self::STAT, 'Comment', 'post_id',
+          'condition' => 'status='.Comment::STATUS_APPROVED),   
 		);
 	}
 
@@ -117,4 +132,43 @@ class Post extends CActiveRecord
 	{
 		return parent::model($className);
 	}
+  
+  public function getUrl()
+  {
+    return Yii::app()->createUrl('post/view', array(
+      'id'=>$this->id,
+      'title'=>$this->title,
+    ));
+  }
+  
+  protected function beforeSave()
+  {
+    if(parent::beforeSave())
+    {
+      if($this->isNewRecord)
+      {
+        $this->create_time=$this->update_time=time();
+        $this->author_id=Yii::app()->user->id;
+      }
+      else
+        $this->update_time=time();
+      return true;
+    }
+    else
+      return false;
+  }
+  
+  protected function afterSave()
+  {
+    parent::afterSave();
+    //Tag::model()->updateFrequency($this->_oldTags, $this->tags);
+  }
+  
+  private $_oldTags;
+  
+  protected function afterFind()
+  {
+    parent::afterFind();
+    $this->_oldTags=$this->tags;
+  }
 }
